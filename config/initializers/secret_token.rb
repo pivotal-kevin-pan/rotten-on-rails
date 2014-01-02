1c9246ff3e8df5358abecf33f7896b34bd2a8905
Kevin Pan <xcpankevin@hotmail.com>
2014-01-02 00:45:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenOnRails::Application.config.secret_key_base = '9ced60ad82236fd94849cb2c852fa71ac9952c2ff0779f740236d98d6ff7834fa18652a4604a63d358a125f2c349ec1589149a16120df772de5acc69eeb0586d'
