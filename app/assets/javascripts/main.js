5ee2fd1342590bed6478e0057c6e6e1d641d5c24
Kevin Pan <xcpankevin@hotmail.com>
2014-01-12 14:36:03 -0500

    Moved logic in javascript to controller

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,4 @@
-var selectedMovie = null;
-var blockbusterMovies = null;
-var inTheatreMovies = null;
-var openingMovies = null;
-var searchMovies = null;
+var selectedMovieId;
 var searchString = '';
 var searchPage = -1;
 var searchTotalItems = -1;
@@ -20,8 +16,6 @@ transitionMappings = {
 }
 
 $(function() {
-	var page = $('.pageInfo').data('temp');
-
 	$(window).on("popstate", function(e) {
 	    if (e.originalEvent.state !== null) {
 	    	searchString = e.originalEvent.state.searchString;
@@ -30,7 +24,7 @@ $(function() {
     	}
 	} );
 
-	getPageData(page); // sets searchString and searchPage
+	getPageData();
 
 	window.history.replaceState({page:page, searchString:searchString, searchPage:searchPage}, '', getUrlPathAfterDomain());
 
@@ -41,20 +35,15 @@ function ajaxNavigate(page, transition, browserBackButtonPress) {
 	var publicParams = '';
 	var hiddenParams = '';
 	var request = null;
-	var getPageDataFlag = false;
 	if (page === pageMappings.HOME) {
 		hiddenParams = 'ajax=true';
-		if (blockbusterMovies == null) {
-			getPageDataFlag = true;
-			hiddenParams += '&load=true';
-		}
 		request = $.ajax({
 			url: '/',
 			type: "get",
 			data: publicParams + hiddenParams
 		});	
 	} else if (page === pageMappings.MOVIEINFO) {
-		publicParams = "id=" + selectedMovie.id;
+		publicParams = "id=" + selectedMovieId;
 		hiddenParams = "&ajax=true";
 		request = $.ajax({
 			url: "/" + page,
@@ -62,7 +51,6 @@ function ajaxNavigate(page, transition, browserBackButtonPress) {
 			data: publicParams + hiddenParams
 		});
 	} else if (page === pageMappings.SEARCH) {
-		getPageDataFlag = true;
 		if (searchString) {
 			publicParams = "search=" + searchString + "&page=" + searchPage;
 		}
@@ -84,8 +72,8 @@ function ajaxNavigate(page, transition, browserBackButtonPress) {
 			else ajaxContent = document.getElementById('ajaxContent_2');
 			ajaxContent.style.display = 'block';
 			ajaxContent.innerHTML = response;
+			$('#spinner').remove();
 
-			if (getPageDataFlag) getPageData(page);
 			pageSetup(page, transition);
 			if (publicParams !== '') publicParams = '?' + publicParams;
 			if (!browserBackButtonPress)
@@ -99,9 +87,10 @@ function ajaxNavigate(page, transition, browserBackButtonPress) {
 
 function pageSetup(page, transition) {
 	if (page === pageMappings.HOME) {
-		displayMovieList('blockbusters');
-		displayMovieList('inTheatres');
-		displayMovieList('opening');
+		$( '.movieLink' ).click(function() {
+			selectedMovieId = this.id;
+			changePage(pageMappings.MOVIEINFO, transitionMappings.GENERAL, false);
+		});
 		$( '#searchForm' ).submit(function () {
 			searchString = $( this ).find( 'input' ).val();
 			searchPage = 1;
@@ -110,31 +99,22 @@ function pageSetup(page, transition) {
 		});
 		document.onkeydown = function (e) {};
 	} else if (page === pageMappings.MOVIEINFO){
-		header = $( 'h1' ).html( selectedMovie.title );
-		document.getElementById('image').src = selectedMovie.posters.profile;
-		document.getElementById('runtime').innerHTML = selectedMovie.runtime;
-		document.getElementById('score').innerHTML = selectedMovie.ratings.critics_score;
-		document.getElementById('critics_consensus').innerHTML = selectedMovie.critics_consensus;
-		document.getElementById('synopsis').innerHTML = selectedMovie.synopsis;
 		$( '#backButton' ).click( function () {
 			changePage(pageMappings.HOME, transitionMappings.GENERAL, false);
 		});
 
-		var cast = document.getElementById('cast');
-		for (var i = 0; i < selectedMovie.abridged_cast.length; i++) {
-			var div = document.createElement('div');
-			div.innerHTML = selectedMovie.abridged_cast[i].name;
-			cast.appendChild(div);
-		}
-		$(document).keydown(function (e) {});
+		document.onkeydown = function (e) {};
 	} else if (page === pageMappings.SEARCH) {
 		if (searchString) {
-			displayMovieList('search');
-			if (transition == transitionMappings.GENERAL) setupPagingButtonsAndSetupKeys();
+			setupPagingButtonsAndSetupKeys();
 		} else {
 			$(document).keydown(function (e) {});
 		}
 		$( '#searchForm' ).find('input').val(searchString);
+		$( '.movieLink' ).click(function() {
+			selectedMovieId = this.id;
+			changePage(pageMappings.MOVIEINFO, transitionMappings.GENERAL, false);
+		});
 		$( '#searchForm' ).submit(function () {
 			searchString = $( this ).find( 'input' ).val();
 			searchPage = 1;
@@ -150,78 +130,11 @@ function getUrlPathAfterDomain () {
 	return a.pathname + a.search;
 }
 
-function displayMovieList(type) {
-	var selectedList;
-	if (type === 'blockbusters') selectedList = blockbusterMovies;
-	else if (type === 'inTheatres') selectedList = inTheatreMovies;
-	else if (type === 'opening') selectedList = openingMovies;
-	else if (type === 'search') selectedList = searchMovies;
-	for (var i = 0; i < selectedList.length; i++) {
-		tbody = document.getElementById(type);
-		tr = document.createElement('tr');
-		div = document.createElement('div');
-		thumbnail = document.createElement('img');
-
-		if (type === 'search') thumbnail.src = selectedList[i].posters.thumbnail;
-		else setThumbnailFreshOrRotten(selectedList[i].ratings.critics_rating, thumbnail);
-
-		divArray = createDivArray(5);
-
-		div.id = i;
-
-		$( div ).click(function() {
-			selectedMovie = selectedList[this.id];
-			changePage(pageMappings.MOVIEINFO, transitionMappings.GENERAL, false);
-		});
-
-		var count = 0;
-		divArray[count].appendChild(thumbnail);
-		divArray[count++].className = 'thumbnail';
-		divArray[count].innerHTML = formatTitleEllipsis(type, selectedList[i].title);
-		divArray[count++].className = 'title';
-
-		if (type === 'search') {
-			divArray[count].innerHTML = selectedList[i].year;
-			divArray[count++].className = 'year';
-		}
-
-		if (selectedList[i].ratings.critics_score != '-1') {
-			divArray[count].innerHTML = selectedList[i].ratings.critics_score + '%';
-		}
-		divArray[count++].className = 'score';
-		divArray[count].innerHTML = selectedList[i].mpaa_rating;
-		divArray[count++].className = 'rating';
-
-		for (var y = 0; y < divArray.length; y++) {
-			div.appendChild(divArray[y]);
-		}
-		tr.appendChild(div);
-		tbody.appendChild(tr);
-	}
-}
-
-function getPageData (page) {
-	if (page === pageMappings.HOME) {
-		var blockbusterData = jQuery.parseJSON( $('.blockbusterInfo').data('temp') );
-		var inTheatreData = jQuery.parseJSON( $('.inTheatreInfo').data('temp') );
-		var openingData = jQuery.parseJSON( $('.openingInfo').data('temp') );
-		blockbusterMovies = blockbusterData.movies;
-		inTheatreMovies = inTheatreData.movies;
-		openingMovies = openingData.movies;
-	} else if (page === pageMappings.MOVIEINFO){
-		var selectedMovieData = jQuery.parseJSON( $('.selectedMovieInfo').data('temp') );
-		selectedMovie = selectedMovieData;
-	} else if (page === pageMappings.SEARCH) {
-		// data element lowercases data variable names
-		if (searchString === '') {
-			searchString = $('.searchInfo').data('searchstring');
-			searchPage = $('.searchInfo').data('pagenumber');
-		}
-		// Javascript automatically converts data value to a Javascript object when data value is "{ "movie":[] }"
-		var searchData = $('.searchInfo').data('temp');
-		if ( typeof searchData != 'object' ) searchData = jQuery.parseJSON( $('.searchInfo').data('temp') );
-		searchMovies = searchData.movies;
-		searchTotalItems = searchData.total;
+function getPageData () {
+	page = $('.pageInfo').data('temp');
+	if (page == 'search') {
+		searchString = $('.searchInfo').data('searchstring');
+		searchPage = $('.searchInfo').data('pagenumber');
 	}
 	$( '.infoContainer' ).remove();
 }
@@ -234,63 +147,22 @@ function createDivArray (size) {
 	return array;
 }
 
-function formatTitleEllipsis(type, title) {
-	var TITLE_LENGTH = 0;
-	if (type === 'search') TITLE_LENGTH = 31;
-	else TITLE_LENGTH = 23;
-	if (title.length > TITLE_LENGTH) {
-		title = title.substr(0, TITLE_LENGTH - 1).trim() + '...';
-	}
-	return title;
-}
-
-function setThumbnailFreshOrRotten (rating, thumbnail) {
-	if (rating == "Certified Fresh") {
-		thumbnail.src = '/assets/fresh.png';
-	} else {
-		thumbnail.src = '/assets/rotten.png';
-	}
-}
-
 function setupPagingButtonsAndSetupKeys () {
-	var ul = document.getElementById('paging');
-	var numberOfListElements = $(ul).children('li').size();
-	var numberOfListElementsBeforeCenter = numberOfListElements/2 - 0.5;
-	var totalPages = Math.ceil(searchTotalItems / SEARCH_ITEMS_PER_PAGE);
-	if (totalPages > 25) totalPages = 25;
-	var pageCounter = 1;
-	if (searchPage - numberOfListElementsBeforeCenter > 0 
-		&& searchPage + numberOfListElementsBeforeCenter <= totalPages) {
-		pageCounter = searchPage - numberOfListElementsBeforeCenter;
-	} else if (searchPage - numberOfListElementsBeforeCenter > 0) {
-		pageCounter = totalPages - numberOfListElements + 1;
-		if (pageCounter <= 0) pageCounter = 1;
-		// pageCounter = numberOfListElements - totalPages + 1;
-	} else {
-		pageCounter = 1;
-	}
 	$( '#paging' ).children().each(function() {
-		if (pageCounter <= totalPages) {
-			if (pageCounter == searchPage) this.id = 'selectedPage';
-			else {
-				this.className = 'selectablePage';
-				this.id = ''; 
-				var transferInfo_pageCounter = pageCounter;
-				this.onclick = function () {
-					searchPage = transferInfo_pageCounter;
-					setupPagingButtonsAndSetupKeys();
-					changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false);
-				};
-			}
-			this.innerHTML = pageCounter++;
+		if (this.className == 'selectablePage') {
+			this.onclick = function () {
+				searchPage = this.innerHTML.trim();
+				changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false);
+			};
 		}
 	});
 	var keyDownFunction;
-	if (searchPage < totalPages && searchPage > 1) {
+	positionOfSelectedPageButton = $('#selectedPage').index();
+	if (0 < positionOfSelectedPageButton && $('.selectablePage').size() > positionOfSelectedPageButton) {
 		keyDownFunction = bothKeysEnable;
-	} else if (searchPage > 1) {
+	} else if (0 < positionOfSelectedPageButton) {
 		keyDownFunction = leftKeyEnable;
-	} else if (searchPage < totalPages) {
+	} else if ($('.selectablePage').size() > positionOfSelectedPageButton) {
 		keyDownFunction = rightKeyEnable;
 	} else {
 		keyDownFunction = function (e) {};
@@ -301,7 +173,6 @@ function setupPagingButtonsAndSetupKeys () {
 function leftKeyEnable (e) {
     if (e.keyCode == 37) {
     	searchPage -= 1;
-    	setupPagingButtonsAndSetupKeys();
 		changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false);
     }
 }
@@ -309,7 +180,6 @@ function leftKeyEnable (e) {
 function rightKeyEnable(e) {
     if (e.keyCode == 39) {
         searchPage += 1;
-        setupPagingButtonsAndSetupKeys();
 		changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false);  
     }	
 }
@@ -317,12 +187,10 @@ function rightKeyEnable(e) {
 function bothKeysEnable(e) {
     if (e.keyCode == 37) { 
        	searchPage -= 1;
-       	setupPagingButtonsAndSetupKeys();
 		changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false);
     }
     if (e.keyCode == 39) {
         searchPage += 1;
-        setupPagingButtonsAndSetupKeys();
 		changePage(pageMappings.SEARCH, transitionMappings.SEARCH2SEARCH, false); 
     }
 }
@@ -331,6 +199,10 @@ function changePage (page, transition, browserBackButtonPress) {
 	var ajaxContentDivId = '#ajaxContent';
 	if (transition == transitionMappings.SEARCH2SEARCH) ajaxContentDivId = '#ajaxContent_2';
 	$( ajaxContentDivId ).fadeOut( 120, function() {
+		spinner = document.createElement('img');
+		spinner.id = 'spinner';
+		spinner.src = '/assets/ajax-loader.gif';
+		document.body.appendChild(spinner);
 		ajaxNavigate(page, transition, browserBackButtonPress);
-	} );	
+	} );
 }
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
 	@@inTheatreMovies = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?page_limit=7&page=1&country=ca&apikey='
 	@@openingMovies = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?limit=7&country=ca&apikey='
 	@@idSearchMovies = ['http://api.rottentomatoes.com/api/public/v1.0/movies/', '.json?apikey=']
-	@@stringSearchMovies = ['http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=', '&page_limit=7&page=', '&apikey=']
+	@@stringSearchMovies = ['http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=', '&page_limit=', '&page=', '&apikey=']
 
 	before_filter :requireNetHttp, :only => [:home, :movieInfo]
 
@@ -13,15 +13,18 @@ class MainController < ApplicationController
 	end
 
 	def home
-		if params[:ajax] != 'true' or params[:load] == 'true'
-			uri = URI(@@blockBusterMovies + @@key)
-			@block = Net::HTTP.get(uri)
-			uri = URI(@@inTheatreMovies + @@key)
-			@theatre = Net::HTTP.get(uri)
-			uri = URI(@@openingMovies + @@key)
-			@opening = Net::HTTP.get(uri)
-			@page = ''
-		end
+		@TITLE_LENGTH = 22
+
+		jsonTemp = getResponseAndJSONDecode(@@blockBusterMovies + @@key)
+		@blockJSON = jsonTemp['movies']
+
+		jsonTemp = getResponseAndJSONDecode(@@inTheatreMovies + @@key)
+		@theatreJSON = jsonTemp['movies']
+
+		jsonTemp = getResponseAndJSONDecode(@@openingMovies + @@key)
+		@openingJSON = jsonTemp['movies']
+
+		@page = ''
 
 		if params[:ajax] == 'true'
 			render :layout => false
@@ -29,49 +32,117 @@ class MainController < ApplicationController
 	end
 
 	def movieInfo
-		if params[:ajax] != 'true'
-			uri = URI.parse(@@idSearchMovies[0] + params[:id] + @@idSearchMovies[1] + @@key)
-			@req = Net::HTTP.get_response(uri)
+		@movieInfo = getResponseAndJSONDecode(
+			@@idSearchMovies[0] + 
+			params[:id] + 
+			@@idSearchMovies[1] + 
+			@@key)
 
-			if (@req["content-encoding"] == 'gzip')
-				@req = Zlib::GzipReader.new(StringIO.new(@req.body)).read
-			else
-				@req = @req.body
-			end
-			@page = 'movieInfo'
-		else
+		@invalidMovieId = determineInvalidMovieId
+
+		if @invalidMovieId
+			@movieInfo['title'] = 'Invalid Movie Id'
+		end
+
+		@page = 'movieInfo'
+
+		if params[:ajax] == 'true'
 			render :layout => false
 		end
 	end
 
 	def search
-		if (params.has_key?(:page))
-			pageNumber = params[:page]
-		else
-			pageNumber = '1'
-		end
-		if (params.has_key?(:search))
-			uri = URI(@@stringSearchMovies[0] + URI.escape(params[:search]) + @@stringSearchMovies[1] + pageNumber + @@stringSearchMovies[2] + @@key)
-			@search = Net::HTTP.get(uri)
-			@searchString = params[:search]
-			@page = 'search'
-			@pageNumber = pageNumber
-			@sendData = false;
+		@ITEMS_PER_PAGE = 7
+		@TITLE_LENGTH = 31
+		@NUMBER_OF_PAGE_BUTTONS = 11
+		@totalPages = 0
+		@pageNumber = determinePageNumber
 
+		if (params.has_key?(:search))
+			@searchString = params[:search]
+			jsonTemp = getResponseAndJSONDecode(
+				@@stringSearchMovies[0] + 
+				URI.escape(@searchString) + 
+				@@stringSearchMovies[1] + 
+				@ITEMS_PER_PAGE.to_s + 
+				@@stringSearchMovies[2] +
+				@pageNumber + 
+				@@stringSearchMovies[3] + 
+				@@key)
+			if jsonTemp.has_key?('movies')
+				@searchJSON = jsonTemp['movies']
+				@totalPages = determineTotalPages(jsonTemp['total'].to_f)
+			else
+				@searchJSON = Array.new
+				@totalPages = 0
+				@pageNumber = -1
+			end
+			@startingPageToDisplaySearchPageLinks = determineStartingPageToDisplaySearchPageLinks
+			@page = 'search'
 			if params[:ajax] == 'true'
 				if (params.has_key?(:partial))
-					@sendData = true;
-					render partial: "shared/searchTable"
+					render partial: 'shared/searchTable'
 				else
 					render :layout => false
 				end
 			end
 		else
-			@search = '{ "movies":[] }'
 			@searchString = ''
 			@page = 'search'
-			@pageNumber = '-1'
 			render 'searchCenter'
+		end
+	end
+
+	def determineStartingPageToDisplaySearchPageLinks
+		numberOfListElementsBeforeCenter = (@NUMBER_OF_PAGE_BUTTONS / 2).floor
+		currentPage = @pageNumber.to_i
+		if currentPage - numberOfListElementsBeforeCenter > 0 and currentPage + numberOfListElementsBeforeCenter <= @totalPages 
+			start = currentPage - numberOfListElementsBeforeCenter
+		elsif currentPage - numberOfListElementsBeforeCenter > 0
+			start = @totalPages - @NUMBER_OF_PAGE_BUTTONS + 1;
+			if start <= 0 
+				start = 1
+			end
+		else
+			start = 1
+		end
+
+		return start
+	end
+
+	def getResponseAndJSONDecode (uriString)
+		uri = URI.parse(uriString)
+		response = Net::HTTP.get_response(uri)
+
+		if (response["content-encoding"] == 'gzip')
+			response = Zlib::GzipReader.new(StringIO.new(response.body)).read
+		else
+			response = response.body
+		end
+		return ActiveSupport::JSON.decode(response)
+	end
+
+	def determinePageNumber
+		if (params.has_key?(:page))
+			return pageNumber = params[:page]
+		else
+			return pageNumber = '1'
+		end		
+	end
+
+	def determineTotalPages(totalItems)
+		totalPages = (totalItems / @ITEMS_PER_PAGE).ceil
+		if totalPages > 25
+			totalPages = 25
+		end
+		return totalPages
+	end
+
+	def determineInvalidMovieId
+		if @movieInfo.has_key?('id')
+			return false
+		else
+			return true
 		end
 	end
 end
